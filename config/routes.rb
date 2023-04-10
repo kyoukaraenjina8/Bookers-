484178722fb1ca4eb14e3d4342389e2d97fe57bd
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-04-10 18:19:45 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/show'
+  get 'books/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
