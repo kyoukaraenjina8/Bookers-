876a4ec8dbcef57c6d12c5caece2267bd2386aed
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-04-13 04:49:41 +0000

    process5バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
-
+  
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def  create
-    book = Book.new(book_params)
-    flash[:notice] = "Book was successfully created."
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -21,10 +26,14 @@ class BooksController < ApplicationController
 
   end
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "book was successfully updated."
     redirect_to book_path(book.id)
+    else
+    @books = Book.all
+    render :edit
+    end
   end
   def destroy
     book = Book.find(params[:id])
